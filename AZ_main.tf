8ac0f64c4a99a50b3bdee0812930af4cfefc6343
hemavardhan-midhtech <hemavardhan.midhtech@gmail.com>
2026-05-30 22:59:37 +0530

    1st commit

---
+++ AZ_main.tf
@@ -0,0 +1,77 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform"
+  location = "East US"
+} 
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-terraform"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-terraform"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-terraform"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  ip_configuration {
+    name                          = "ipconfig-terraform"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "stterraform"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+} 
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "pip-terraform"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "vm-terraform"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "osdisk-terraform"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "vm-terraform"
+    admin_username = "adminuser"
+    admin_password = "P@ssw0rd1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
